48d3bccc83baab28d5ad9aeef860f3395e4a6708
Giovanni Delussu <surfer@crs4.it>
2025-06-24 13:48:41 +0200

    make backend accessible from not only localhost and set factory as app is returned by method create_app

--- backend-fastapi/app/__main__.py
+++ backend-fastapi/app/__main__.py
@@ -2,8 +2,12 @@ import uvicorn
 from app.app_factory import create_app
 
 if __name__ == "__main__":
-    app=create_app()
-    uvicorn.run("app.app_factory:create_app", host="127.0.0.1", port=5000, lifespan="on",reload=True)
-
-
-
+    app = create_app()
+    uvicorn.run(
+        "app.app_factory:create_app",
+        host="0.0.0.0",
+        port=5000,
+        lifespan="on",
+        reload=True,
+        factory=True,
+    )
